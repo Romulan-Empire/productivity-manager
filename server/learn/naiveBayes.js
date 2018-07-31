c35a313bf091ac0ddb9a91991ac9bef20ba8758d
brianlee <blees.email@gmail.com>
2018-07-30 22:19:05 -0400

    Refactor machine learning process into a class

--- server/learn/naiveBayes.js
+++ server/learn/naiveBayes.js
@@ -1,19 +1,8 @@
 const bayes = require('classificator');
-let classifier = bayes();
-
 const moment = require('moment');
 const { CronJob } = require('cron');
 const { getBrowserActivities } = require('../database/index.js');
 
-const syncClassifierToDb = new CronJob({
-  cronTime: '0 * * * *', //every hour at minute zero
-  onTick: () => {
-    console.log('syncing classifier to DB at', moment().format('MMMM Do YYYY, h:mm:ss a'))
-    initClassifier()
-  },
-  start: true,
-  timeZone: 'America/New_York'
-});
 
 class Classifier {
   constructor() {
@@ -26,9 +15,9 @@ class Classifier {
     const activities = await getBrowserActivities();
     try {
       this.bulkLearn(activities);
-      console.log('Capital C Classifier initialized!');
+      console.log('classifier initialized!');
     } catch(e) {
-      console.error('error in trying to run capital C naive-bayes', e)
+      console.error('error in trying to initilize classifier', e);
     }
   }
 
@@ -42,7 +31,7 @@ class Classifier {
   setSyncCronJob() {
     console.log('setting new cron job')
     new CronJob({
-      cronTime: '* * * * *', //every hour at minute zero
+      cronTime: '0 * * * *', //every hour at minute zero
       onTick: () => {
         console.log('syncing classifier to DB at', moment().format('MMMM Do YYYY, h:mm:ss a'))
         this.init()
@@ -59,7 +48,7 @@ class Classifier {
     else return null;
   }
 
-  earnProductivityClass(title, productivityClass) {
+  learnProductivityClass(title, productivityClass) {
     this.bayes.learn(title, productivityClass);
     console.log(`ml is learning that ${title} is ${productivityClass}`);
   };
@@ -71,46 +60,3 @@ class Classifier {
 }
 
 exports.classifier = new Classifier();
-
-
-const initClassifier = async () => {
-  classifier = null;
-  classifier = bayes();
-  const activities = await getBrowserActivities();
-  try {
-    bulkLearn(activities);
-    console.log('classifier initialized!');
-  } catch(e) {
-    console.error('error in trying to run naive-bayes', e)
-  }
-};
-
-const bulkLearn = (activities) => {
-  activities.filter(({ app_name, prod_class }) => app_name === 'Google Chrome' && prod_class !== 'neutral')
-            .forEach(({window_title, prod_class}) => {
-              classifier.learn(window_title, prod_class);
-            });
-};
-
-const REQUIRED_CONFIDENCE = 0.9;
-const predictProductivityClass = (title, user_name) => {
-  const prediction = classifier.categorize(title).likelihoods
-                                .filter(cat => cat.proba > REQUIRED_CONFIDENCE)
-  if (prediction.length) return prediction[0].category;
-  else return null;
-};
-
-const learnProductivityClass = (title, productivityClass) => {
-  classifier.learn(title, productivityClass);
-  console.log(`ml is learning that ${title} is ${productivityClass}`);
-};
-
-const unlearnProductivityClass = (title, oldProdClass) => {
-  classifier.unlearn(title, oldProdClass);
-  console.log(`ml is UNlearning that ${title} was classified ${oldProdClass}`);
-};
-
-exports.initClassifier = initClassifier;
-exports.predictProductivityClass = predictProductivityClass;
-exports.learnProductivityClass = learnProductivityClass;
-exports.unlearnProductivityClass = unlearnProductivityClass;
