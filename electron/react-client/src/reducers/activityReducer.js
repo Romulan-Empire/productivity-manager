2aeab4ca61d59d4c754b5dcda3e0294f102fdc89
brianlee <blees.email@gmail.com>
2018-08-04 22:15:05 -0400

    Babel config to run mocha tests

--- electron/react-client/src/reducers/activityReducer.js
+++ electron/react-client/src/reducers/activityReducer.js
@@ -14,6 +14,7 @@ const activities = (state = initialState, action) => {
   switch (action.type) {
     case ADD_ACTIVITY: {
       const {app, title, startTime, endTime, productivity} = action.payload
+      console.log('action.payload is', JSON.stringify(action.payload))
       const duration = getDuration(startTime, endTime)
       const convertedProd = {
         ...productivity,
--- electron/react-client/src/reducers/activityReducer.spec.js
+++ electron/react-client/src/reducers/activityReducer.spec.js
@@ -1,4 +1,5 @@
-import activities from './activityReducer.js';
+const activities = require('./activityReducer.js');
+const { getDuration } = require('../constants.js');
 
 describe('addActivity', () => {
   beforeEach(function() {
@@ -11,16 +12,43 @@ describe('addActivity', () => {
       distracting: [],
       nextId: 1
     };
+
     const action = {
       type: 'ADD_ACTIVITY',
       payload: {
-        app: '',
-        title: '',
-        startTime: '',
-        endTime: '',
-        productivity: {}
+        "app":"Google Chrome",
+        "endTime":"August 2nd 2018, 9:32:45 pm",
+        "id":3787,
+        "productivity":{
+          "class":"productive",
+          "source":"ml"
+        },
+        "startTime":"August 2nd 2018, 9:32:44 pm",
+        "title":"npm install broken · Issue #665 · ssbc/patchwork"
       }
     };
-    assert.equal(true, true);
+
+    const newData = {
+      id: 1,
+      app: 'Google Chrome',
+      title: 'npm install broken · Issue #665 · ssbc/patchwork',
+      spurts: [
+        {startTime: 'August 2nd 2018, 9:32:44 pm'},
+        {endTime: 'August 2nd 2018, 9:32:45 pm'}
+      ],
+      duration: getDuration("August 2nd 2018, 9:32:44 pm", "August 2nd 2018, 9:32:45 pm"),
+      productivity: {
+        class: 'productive',
+        source: 'ml'
+      }
+    };
+
+    const newState = {
+      neutral: [],
+      productive: [newData],
+      distracting: [],
+      nextId: 2
+    };
+    assert.deepEqual(activities(initialState, action), newState);
   });
 });
